026ff665083e7cf6343bc107df61ff701cad321b
Tom McClintock <tmcclintock89@gmail.com>
2020-09-11 10:02:45 -0700

    Wrote the setup.py file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="TemplateForDataScience",
+    author="Tom McClintock",
+    author_email="thmsmcclintock@gmail.com",
+    url="https://github.com/tmcclintock/TemplateForDataScience",
+    packages=["insight"],
+)
